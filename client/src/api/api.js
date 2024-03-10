fa73e5d099729de7d4f636aa7b0a66ee317caff1
TzviGreenfeld <tzvigreenfield@gmail.com>
2024-03-11 00:11:08 +0200

    extract our api to function

---
+++ client/src/api/api.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+
+export const api = axios.create({
+  baseURL: process.env.REACT_APP_API_URI,
+});
+
+export const createOrUpdateUser = async (user) => {
+  const token = localStorage.getItem("access_token");
+  const refresh_token = localStorage.getItem("refresh_token");
+  return api.post(
+    "/users",
+    {
+      user: {
+        display_name: user.display_name,
+        spotifyId: user.id,
+        email: user.email,
+      },
+      refreshToken: refresh_token,
+    },
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  );
+};
