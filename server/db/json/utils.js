6586cb5d699e7e5dd8bcc70c61d5e0e8eb0f3f3e
TzviGreenfeld <tzvigreenfield@gmail.com>
2024-04-09 19:03:57 +0300

    fix path

--- server/db/json/utils.js
+++ server/db/json/utils.js
@@ -1,9 +1,11 @@
 import dotenv from "dotenv";
 import { JSONFilePreset } from "lowdb/node";
 dotenv.config("../.env");
+import path from "path";
 
 const __dirname = new URL(".", import.meta.url).pathname;
-const db = await JSONFilePreset(`${__dirname}/db.json`, { userToTokens: [] });
+const dbPath = path.join(__dirname, "db.json");
+const db = await JSONFilePreset(dbPath, { userToTokens: [] });
 await db.read();
 
 const { userToTokens } = db.data;
